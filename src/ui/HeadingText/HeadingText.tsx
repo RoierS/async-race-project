a55e58055fdcb5f93845001291198588ad1a65fc
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-04-16 14:11:25 +0200

    feat: add basic fonts

--- src/ui/HeadingText/HeadingText.tsx
+++ src/ui/HeadingText/HeadingText.tsx
@@ -1,5 +1,12 @@
+import styles from "./HeadingText.module.css";
+
 function HeadingText() {
-  return <h1>Async Race Underground II</h1>;
+  return (
+    <div>
+      <h1 className={styles.title}>Async Race</h1>
+      <h2 className={styles.text}>Most wanted</h2>
+    </div>
+  );
 }
 
 export default HeadingText;
