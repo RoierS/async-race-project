e0392424df8cdf4dc5ad8120cbd9e0ccc56d1d57
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-04-17 20:43:43 +0200

    feat: create app logo component

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -1,3 +1,5 @@
+import Logo from "@ui/Logo/Logo";
+
 import HeaderNav from "../HeaderNav/HeaderNav";
 import HeadingText from "../HeadingText/HeadingText";
 
@@ -6,7 +8,10 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.header}>
+      <Logo />
+
       <HeadingText />
+
       <HeaderNav />
     </header>
   );
