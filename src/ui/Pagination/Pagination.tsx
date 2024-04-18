82219e2f17aff73976f23208cdb198b98af3cb82
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-04-18 09:48:14 +0200

    feat: build static pagination component

--- src/ui/Pagination/Pagination.tsx
+++ src/ui/Pagination/Pagination.tsx
@@ -1,10 +1,30 @@
+import Button from "@ui/Button/Button";
+
+import styles from "./Pagination.module.css";
+
 function Pagination() {
+  const handleNextClick = () => {
+    // TODO
+  };
+
+  const handlePrevClick = () => {
+    // TODO
+  };
+
   return (
-    <div>
-      <button type="button">Prev</button>
-      <p>Page #1</p>
-      <p>Garage (303)</p>
-      <button type="button">Next</button>
+    <div className={styles.pagination}>
+      <Button type="button" onClick={handlePrevClick}>
+        Prev
+      </Button>
+      <p className={styles.pageNumber}>
+        Page: <span className={styles.currentNumber}> #1</span>
+      </p>
+      <p className={styles.carsCount}>
+        Garage: <span className={styles.currentNumber}>303</span>
+      </p>
+      <Button type="button" onClick={handleNextClick}>
+        Next
+      </Button>
     </div>
   );
 }
