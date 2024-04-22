238eb534d9bbf6700f5bebb2c938f2793b8f0f96
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-04-22 16:15:12 +0200

    build: install and configure react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,32 @@ import Winners from "@pages/Winners/Winners";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
-const staleTime = 60000;
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime,
+      staleTime: 0,
     },
   },
 });
+
+const toastOptions = {
+  success: {
+    duration: 3000,
+  },
+  error: {
+    duration: 5000,
+  },
+  style: {
+    fontSize: "16px",
+    maxWidth: "500px",
+    padding: "16px 24px",
+    backgroundColor: "var(--color-ligh-1)",
+    color: "var(--rich-black)",
+  },
+};
 
 function App() {
   return (
@@ -31,6 +46,13 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={toastOptions}
+      />
     </QueryClientProvider>
   );
 }
