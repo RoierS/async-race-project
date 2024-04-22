9b3f1323fc74abcf1da43ebf040b748f6a99bf83
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-04-22 10:59:47 +0200

    feat: encapsulate error messages within an object

--- src/services/apiRequest.ts
+++ src/services/apiRequest.ts
@@ -1,8 +1,10 @@
 import { HttpMethod, BASE_URL } from "@constants/constants";
 
-const ErorrCodes = {
-  TOO_MANY_REQUESTS_ERROR_CODE: 429,
-  BROKEN_ENGINE_ERROR_CODE: 500,
+const GarageErrorMassage = {
+  400: `Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive`,
+  404: `Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?`,
+  429: "Drive already in progress. You can't run drive for the same car twice while it's not stopped.",
+  500: "Car has been stopped suddenly. It's engine was broken down.",
 };
 
 export default async function request<T>(
@@ -15,7 +17,6 @@ export default async function request<T>(
     method,
     headers: {
       "Content-Type": "application/json",
-      "X-Total-Count": "4",
     },
     signal,
   };
@@ -26,15 +27,11 @@ export default async function request<T>(
   const response = await fetch(`${BASE_URL}${url}`, options);
 
   if (!response.ok) {
-    if (response.status === ErorrCodes.TOO_MANY_REQUESTS_ERROR_CODE) {
-      throw new Error("Too many requests. Please try again later.");
-    } else if (response.status === ErorrCodes.BROKEN_ENGINE_ERROR_CODE) {
-      throw new Error(
-        "Car has been stopped suddenly. It's engine was broken down.",
-      );
-    } else {
-      throw new Error(`Request failed. Status: ${response.status}`);
-    }
+    const { status } = response;
+    const errorMessage =
+      (GarageErrorMassage as Record<number, string>)[status] ||
+      `Request failed. Status: ${status}`;
+    throw new Error(errorMessage);
   }
 
   const data: T = await response.json();
