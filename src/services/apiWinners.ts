58d775777f5ef0adeb0c65a34e2d8156274c4a84
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-04-26 18:55:54 +0200

    feat: add api get winner by id

--- src/services/apiWinners.ts
+++ src/services/apiWinners.ts
@@ -1,11 +1,14 @@
+import { HttpMethod } from "@constants/constants";
 import { Car } from "@interfaces/Car";
 
 import request from "./apiRequest";
 
-export default async function getWinners(
-  page: number,
-  limit: number,
-): Promise<Car[]> {
+export async function getWinners(page: number, limit: number) {
   const data: Car[] = await request(`/winners?_page=${page}&_limit=${limit}`);
+  return data;
+}
+
+export async function getWinner(id: number) {
+  const data: Car = await request(`/winners/${id}`, HttpMethod.GET);
   return data;
 }
