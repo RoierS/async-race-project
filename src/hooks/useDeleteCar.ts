313b7f7ad539c2f8e15f422816d09db7c0dae6a3
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-04-27 18:44:00 +0200

    feat: load prev page when delete last car on page

--- src/hooks/useDeleteCar.ts
+++ src/hooks/useDeleteCar.ts
@@ -1,14 +1,41 @@
-import { deleteCar } from "@services/apiGarage";
+import { PAGE_SIZE } from "@constants/constants";
+import { deleteCar, getCars, getTotalCarCount } from "@services/apiGarage";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useSearchParams } from "react-router-dom";
 
 const useDeleteCabin = () => {
   const queryClient = useQueryClient();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const { isPending: isDeleting, mutate: deleteExistingCar } = useMutation({
     mutationFn: deleteCar,
-    onSuccess: () => {
+    onSuccess: async () => {
       queryClient.invalidateQueries();
+
+      const currentPage = Number(searchParams.get("page")) || 1;
+      const carsCount = await queryClient.fetchQuery({
+        queryKey: ["cars-count"],
+        queryFn: getTotalCarCount,
+      });
+
+      if (
+        carsCount &&
+        Math.ceil(carsCount / PAGE_SIZE) < currentPage &&
+        currentPage > 1
+      ) {
+        searchParams.set("page", String(currentPage - 1));
+        setSearchParams(searchParams);
+
+        queryClient.invalidateQueries({
+          queryKey: ["cars", carsCount],
+        });
+
+        queryClient.prefetchQuery({
+          queryKey: ["cars", currentPage - 1],
+          queryFn: () => getCars({ page: currentPage - 1 }),
+        });
+      }
 
       toast.success("Car deleted");
     },
